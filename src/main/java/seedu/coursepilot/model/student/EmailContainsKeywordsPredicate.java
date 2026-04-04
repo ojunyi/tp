578a9a0ba8e38a9c2afeee345a63acfd1132d57d
JY <junyi4446@gmail.com>
2026-04-04 11:05:04 +0800

    Add StudentFieldPredicate to follow OOP

--- src/main/java/seedu/coursepilot/model/student/EmailContainsKeywordsPredicate.java
+++ src/main/java/seedu/coursepilot/model/student/EmailContainsKeywordsPredicate.java
@@ -1,44 +1,26 @@
 package seedu.coursepilot.model.student;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 import seedu.coursepilot.commons.util.StringUtil;
-import seedu.coursepilot.commons.util.ToStringBuilder;
 
 /**
- * Tests that a {@code Student}'s {@code Email} matches any of the keywords given.
+ * Tests that a {@code Student}'s {@code Email} contains any of the given keywords.
+ * Matching is case-insensitive and checks for substring presence.
  */
-public class EmailContainsKeywordsPredicate implements Predicate<Student> {
-    private final List<String> keywords;
+public class EmailContainsKeywordsPredicate extends StudentFieldPredicate {
 
+    /**
+     * Creates an {@code EmailContainsKeywordsPredicate} with the given {@code keywords}.
+     */
     public EmailContainsKeywordsPredicate(List<String> keywords) {
-        this.keywords = keywords;
+        super(keywords);
     }
 
     @Override
     public boolean test(Student student) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsPartWordIgnoreCase(student.getEmail().toString(), keyword));
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        // instanceof handles nulls
-        if (!(other instanceof EmailContainsKeywordsPredicate)) {
-            return false;
-        }
-
-        EmailContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (EmailContainsKeywordsPredicate) other;
-        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this).add("keywords", keywords).toString();
+                .anyMatch(keyword ->
+                        StringUtil.containsPartWordIgnoreCase(student.getEmail().toString(), keyword));
     }
 }
